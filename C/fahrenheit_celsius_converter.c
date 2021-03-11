b7ec4a8f3a172611bd7e73f53af24a98767e17d1
quinwoods <qlyleswoods@protonmail.com>
2021-03-11 17:09:29 -0500

    version two

--- C/fahrenheit_celsius_converter.c
+++ C/fahrenheit_celsius_converter.c
@@ -9,11 +9,20 @@ upper = 300;
 step = 20; 
 
 fahrenheit = lower; 
-
+printf("Fahrenheit\t Celcius\n");
 while (fahrenheit <= upper){
 celsius = 5 * (fahrenheit - 32) / 9;
-printf("%d\t%d\n", fahrenheit, celsius);
+printf("%d\t\t%d\n", fahrenheit, celsius);
 fahrenheit += step; 
+}
+celsius = lower;
+printf("Celcius\t\tFahrenheit\n");
+
+while (celsius <= upper){
+fahrenheit = celsius * 9/5 + 32;
+printf("%d\t\t%d\n", celsius,fahrenheit);
+celsius += step;
+
 }
 return 0;
 }
