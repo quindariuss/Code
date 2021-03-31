5c6f58d2bd566796d36fb97b9909c305e34c3219
quinwoods <qlyleswoods@protonmail.com>
2021-03-31 01:12:02 -0400

    intial commit

--- Swift/linked_list.swift
+++ Swift/linked_list.swift
@@ -1,70 +1,27 @@
-public class Node<T>
-	{
-	var value: T
-	var next: Node?
-	wear var previous: Node?
+ class LinkedListNode<String> {
+  var value: String
+  var next: LinkedListNode?
+  weak var previous: LinkedListNode?
 
-	public init(value: T)
-		
-		{
-		self.value = value
-		}
-	}
-public class linked_list<T>
-	{
-	private var head: Node?
-	public var empty: Bool
-		{
-		return head == nil
-		}
-	public var first: Node? 
-		{
-		return head
-		}
-	public var last: Node?
-		{
-		guard var node = head else 
-			{
-			return nil
-			}
-		while let next = node.next
-			{
-			node = next
-			}
-		return node
-		}
-	public var count: Int
-		{
-		guard var node = head else
-			{
-			return 0
-			}
-		var count = 1 
-		while let next = node.next
-			{
-			node = next 
-			count += 1 
-			}
-		return count
-		}
-	public var print: String
-		{
-		var s = "["
-		var node = head 
-	
-		}
-	public func append(value: T)
-		{
-		let new_node = Node(value: value)
-		if let last_node = last 
-			{
-			new_node.previous = last_node
-			last_node.next = new_node
-				}
-		else
-			{
-			head = new_node
-			}
-		}
-	}
+   init(value: String) {
+    self.value = value
+  }
+}
+ class LinkedList<String> {
+   typealias Node = LinkedListNode<String>
 
+   var head: Node?
+
+   var isEmpty: Bool {
+    return head == nil
+  }
+
+   var first: Node? {
+    return head
+  }
+}
+
+//Running Program 
+
+let list = LinkedList<String>()
+print("\(list.isEmpty) \(list.first)")
