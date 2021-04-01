02c209915e4b1b92841795bc5df41c712af30d15
quinwoods <qlyleswoods@protonmail.com>
2021-04-01 18:34:41 +0000

    adding modules

---
+++ Elxir/geometery.ex
@@ -0,0 +1,14 @@
+defmodule Geometery do
+	def rectangle_area(a,b) do 
+		a * b 
+	end 
+	def square_area do
+		rectangle_area(a, a)
+	end
+end 
+defmodule Rectangle do
+	def area(a), do: area(a,a)
+	
+	def area(a,b, do: a * b
+	end
+end
