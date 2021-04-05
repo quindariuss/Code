53bd308205a39a3fb3e1ba6ee276588563c11c46
quinwoods <qlyleswoods@protonmail.com>
2021-04-05 11:40:47 +0000

    another commit

--- Node/rest/server.js
+++ Node/rest/server.js
@@ -5,9 +5,38 @@ var fs = require("fs");
 app.get('/agelimitforpuppies', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
 	  console.log( data );
+	  console.log("I have been summoned")
 	  res.end( data );
    });
 })
+
+app.get('/puppies', function (req, res) {
+	   fs.readFile( __dirname + "/" + "puppyvistors.json", 'utf8', function (err, data) {
+		  console.log( data );
+		  console.log("I have been summoned")
+		  res.end( data );
+	   });
+	})
+	
+
+var puppy = {
+	"puppy" : 
+	{
+		"name" : "octavia"
+	},
+}
+
+app.post('/addpuppy', function (req, res) {
+	   // First read existing users.
+	   fs.readFile( __dirname + "/" + "puppyvistors.json", 'utf8', function (err, data) {
+		  data = JSON.parse( data );
+		  data["puppy"] = puppy["puppy"];
+		  console.log( data );
+		  res.end( JSON.stringify(data));
+	   });
+	})
+
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address
