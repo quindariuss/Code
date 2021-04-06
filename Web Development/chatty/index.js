204948495cb913dcdf6dcf266e4ad3b1ca8ce0ec
quinwoods <qlyleswoods@protonmail.com>
2021-04-06 17:11:10 +0000

    almost done

--- Web Development/chatty/index.js
+++ Web Development/chatty/index.js
@@ -1,9 +1,20 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/',(req, res) => 
 {
-	res.send('<h1>Hello World</h1>');
+	res.sendFile(__dirname + '/index.html');
+}
+);
+
+io.on('connection', (socket)=>
+{
+	socket.on('chat message', (msg) =>
+	{
+		io.emit('chat message', msg);
+	}
+	);	
 }
 );
 
