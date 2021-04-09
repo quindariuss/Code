a427a47c4bf04c1eeda6a1d0705d0c4afacee536
Quin’darius Lyles-Woods <quindarius@quindariuss-macbook-pro.local>
2021-04-09 22:42:42 +0000

    props

--- React/hello_world/src/index.js
+++ React/hello_world/src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-function tick() {
-	  const element = (
-		<div>
-		  <h1>Hello, world!</h1>
-		  <h2>It is {new Date().toLocaleTimeString()}.</h2>
-		</div>
-	  );
-	  ReactDOM.render(element, document.getElementById('root'));
+function Welcome(props) {
+	  return <h1>Hello, {props.name}</h1>;
 	}
 	
-	setInterval(tick, 1);
+	function App() {
+	  return (
+		<div>
+		  <Welcome name="Sara" />
+		  <Welcome name="Cahal" />
+		  <Welcome name="Edite" />
+		</div>
+	  );
+	}
 	
+	ReactDOM.render(
+	  <App />,
+	  document.getElementById('root')
+	);
