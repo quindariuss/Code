6798893c8ecf7b87bb6eb73a1f4c1e6f0e261292
Quin’darius Lyles-Woods <quindarius@quindariuss-macbook-pro.local>
2021-04-12 18:51:52 +0000

    couldnt really understand props and componets here

--- Web Development/showing_files/src/index.js
+++ Web Development/showing_files/src/index.js
@@ -1,21 +1,38 @@
 import react from 'react';
 import ReactDOM from 'react-dom';
 
-function Hello(props) {
-	  return<h1>Hello, {props.name}</h1>;
-	}
-	
-	function Greet() {
+function Comment(props) {
 	  return (
-		<div>
-		  <Hello name="Sara" />
-		  <Hello name="Cahal" />
-		  <Hello name="Edite" />
+		<div className="Comment">
+		  <div className="UserInfo">
+			<Avatar />
+			<div className="UserInfo-name">
+			  {props.author.name}
+			</div>
+		  </div>
+		  <div className="Comment-text">
+			{props.text}
+		  </div>
+		  <div className="Comment-date">
+			{formatDate(props.date)}
+		  </div>
 		</div>
 	  );
 	}
 	
-	ReactDOM.render(
-	  <Greet />,
-	  document.getElementById('root')
+	
+	
+ReactDOM.render
+(
+<h1></h1>,
+document.getElementById('root')
+);
+
+function Avatar(props) {
+    return (
+		<img className="Avatar"
+        src={props.author.avatarUrl}
+        alt={props.author.name} 
+		/>
 	);
+}
