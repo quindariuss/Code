23152a0758ca3616e98c9c61281f3aad416ea2e7
Quin’darius Lyles-Woods <quindarius@quindariuss-macbook-pro.local>
2021-04-13 18:57:30 +0000

    got an icon to show with a component

--- Web Development/facebook/src/App.js
+++ Web Development/facebook/src/App.js
@@ -4,19 +4,30 @@ function App()
 {
 	return(
 			<Navbar>
-			
+				<NavItem icon = '😀'/>
 			</Navbar>
 	);
 }
 
-function Navbar()
+function Navbar(props)
 {
 	return(
 		<nav className='navbar'>
 			<ul className='navbar-nav'>
-				
+				{props.children}
 			</ul>
 		</nav>
+	);
+}
+
+function NavItem(props)
+{
+	return(
+		<li className='nav-item'>
+			<a href='#' className='icon-button'>
+				{props.icon}
+			</a>
+		</li>
 	);
 }
 
