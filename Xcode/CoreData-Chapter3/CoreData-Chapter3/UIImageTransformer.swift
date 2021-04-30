35cb4900c356ff211245f5d800aabc118ee17ed5
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-04-30 15:35:30 -0400

    finished setting up all the data models

---
+++ Xcode/CoreData-Chapter3/CoreData-Chapter3/UIImageTransformer.swift
@@ -0,0 +1,45 @@
+//
+//  UIImageTransformer.swift
+//  CoreData-Chapter3
+//
+//  Created by Quin'darius Lyles-Woods on 4/30/21.
+//
+
+import Foundation
+import SwiftUI
+import CoreData
+
+class UIImageTransformer: ValueTransformer {
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let image = value as? UIImage
+            else
+        {
+            return nil
+        }
+        do
+        {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: image, requiringSecureCoding: true)
+            return data
+        }
+        catch
+        {
+            return nil
+        }
+    }
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let data = value as? Data
+            else
+        {
+            return nil
+        }
+        do
+        {
+            let image = try NSKeyedUnarchiver.unarchivedObject(ofClass: UIImage.self, from: data)
+            return image
+        }
+        catch
+        {
+            return nil
+        }
+    }
+}
