8f5df7590bc89fd4aa7b829376ff805b4faa7444
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-01 22:02:50 +0000

    little menu with chakrua ui

--- React/chak/src/index.js
+++ React/chak/src/index.js
@@ -1,14 +1,42 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ChakraProvider, Box, Text, Flex, Button} from "@chakra-ui/react"
+import { createBreakpoints} from "@chakra-ui/theme-tools";
+import { ChakraProvider ,Box} from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  ChevronDownIcon,
+  Button,
+  MenuIcon,
+  MenuCommand,
+  MenuDivider,
+} from "@chakra-ui/react"
 
-
+const breakpoints = createBreakpoints({
+  sm: "30em",
+  md: "48em",
+  lg: "62em",
+  xl: "80em",
+  "2xl": "96em",
+})
 
 ReactDOM.render(
- <ChakraProvider>
-  
-<Box w="100%" h="200px" bgGradient="linear(to-r, green.200, pink.500)" />
-</ChakraProvider>,
-  document.getElementById('root')
-);
-
+   <ChakraProvider>
+<Menu isLazy>
+<MenuButton>Open menu</MenuButton>
+<MenuList>
+  {/* MenuItems are not rendered unless Menu is open */}
+  <MenuItem>New Window</MenuItem>
+  <MenuItem>Open Closed Tab</MenuItem>
+  <MenuItem>Open File</MenuItem>
+</MenuList>
+</Menu>
+  </ChakraProvider>,
+    document.getElementById('root')
+  );
