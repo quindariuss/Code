ae2944e09eeb6a0b129404240c71bf64bd7f6715
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-13 13:57:50 +0000

    length of characters in string?

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,6 +1,8 @@
-fn main() {
-    let mut x = 5;
-    let x = x + 1;
-    let x = x * 2;
-    println!("The value of x is {}", x);
+fn main()
+{
+    let spaces = "    ";
+    println!("spaces is {}", spaces);
+    let spaces = spaces.len();
+    println!("spaces is {}", spaces);
+
 }
