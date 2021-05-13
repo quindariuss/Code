6bfbc05d70daf10798da6cfabfe1b34614c40023
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-13 13:49:04 +0000

    CONSTANTS

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let mut x = 5;
+    const MAX_POINTS: u32 = 100_000;
     println!("The value of x is {}", x);
     x = 6;
     println!("The value of x is {}", x);
+    println!("The value of MAX_POINTS is {}", MAX_POINTS);
 }
