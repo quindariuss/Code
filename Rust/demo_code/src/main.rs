b25f0c806eda1211bc7c6db0ac31ed749c2737ee
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-13 14:25:48 +0000

    boolean

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,20 +1,9 @@
 fn main()
 {
-    let sum = 5 + 6;
-    println!("The sum is {}", sum);
+    let t = true;
 
-    let diff = 96.4 - 43.2;
-    println!("The diff is {}", diff);
+    let f: bool = false;
 
-    let product = 4 * 30;
-    println!("The product is {}", product);
-
-    let division = 4.43 / 30.34;
-    println!("The division is {}", division);
-
-    let remainder = 4.43 % 30.34;
-    println!("The remainder is {}", remainder);
-
-
-
+    println!("t is {}", t);
+    println!("f is {}", f);
 }
