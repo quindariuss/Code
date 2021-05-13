1f79cfbfe524b2060d320113d3eea7baeccb4e15
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-13 14:35:23 +0000

    this is what you must do

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,5 +1,6 @@
 fn main()
 {
-    let tup: (i32, f64, u8) = (500, 6.4 1);
-    println!("The tup is {}", tup);
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of x:{}, y:{}, and z:{} of the tuple", x, y, z);
 }
