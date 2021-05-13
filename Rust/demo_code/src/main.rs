3529cd2609719bf6f552b115b226d6df76ea2024
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-13 14:37:55 +0000

    this is how you access the tuple values directly

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -2,5 +2,5 @@ fn main()
 {
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     let (x, y, z) = tup;
-    println!("The value of x:{}, y:{}, and z:{} of the tuple", x, y, z);
+    println!("The value of x:{}, y:{}, and z:{} of the tuple accessing it directly", tup.0, tup.1, tup.2);
 }
