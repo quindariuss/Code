5dccb4647d97a7427bfaec09396faea8d3bee100
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-13 14:24:18 +0000

    Numeric operations

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,8 +1,20 @@
 fn main()
 {
-    let mut spaces = "    ";
-    println!("spaces is {}", spaces);
-    spaces = spaces.len();
-    println!("spaces is {}", spaces);
-    println!("finished");
+    let sum = 5 + 6;
+    println!("The sum is {}", sum);
+
+    let diff = 96.4 - 43.2;
+    println!("The diff is {}", diff);
+
+    let product = 4 * 30;
+    println!("The product is {}", product);
+
+    let division = 4.43 / 30.34;
+    println!("The division is {}", division);
+
+    let remainder = 4.43 % 30.34;
+    println!("The remainder is {}", remainder);
+
+
+
 }
