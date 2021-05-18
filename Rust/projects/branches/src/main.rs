2fc9f2ea894507d11fd5b2247c2f1c7db6de46c8
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-18 14:41:08 +0000

    better syntax

--- Rust/projects/branches/src/main.rs
+++ Rust/projects/branches/src/main.rs
@@ -3,10 +3,8 @@ fn main()
     let a = [10,20,30,40,50];
     let mut index = 0;
 
-    while index < 5
+    for element in a.iter()
     {
-        println!("The value is: {}", a[index]);
-
-        index += 1;
+        println!("The value is: {}", element);
     }
 }
