069f69b00354edc44e7db208b5ce4477b2a1474f
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-18 14:28:02 +0000

    conditials

--- Rust/projects/branches/src/main.rs
+++ Rust/projects/branches/src/main.rs
@@ -1,7 +1,17 @@
 fn main()
 {
+    let mut counter = 0;
+
+    let result =
     loop
     {
-        println!("Again");
-    }
+        counter += 1;
+
+        if counter == 10
+        {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
 }
