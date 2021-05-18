3aafc87cea1a0082561e4e205e966e2ca576bcff
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-18 13:20:57 +0000

    Expressions dont get semicolons and this is a function with parameters

--- Rust/projects/functions/src/main.rs
+++ Rust/projects/functions/src/main.rs
@@ -1,10 +1,11 @@
-fn main() {
-   let x = 5;
-   let y = {
-       let x = 3;
-       x+1
-   };
-
-   println!("The value of y is {}", y);
+fn five() -> i32
+{
+    5
 }
 
+fn main()
+{
+    let x = five();
+
+    println!("The value of x is {}", x);
+}
