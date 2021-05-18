4302e8b8dfd5dfcf7ac35a0bad5ebd4eb8e13595
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-18 13:16:34 +0000

    expression block in rust

--- Rust/projects/functions/src/main.rs
+++ Rust/projects/functions/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
-   let y = 6;
+   let x = 5;
+   let y = {
+       let x = 3;
+       x+1;
+   };
+
+   println!("The value of y is {}", y);
 }
 
-fn another_function(x: i32, y: i32)
-{
-    println!("The value of x is: {}",x);
-    println!("The value of y is: {}",y);
-}
