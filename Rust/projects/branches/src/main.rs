ba6cd051328582af0150a2610cab18cd21dffdec
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-18 14:44:36 +0000

    liftoff a little different

--- Rust/projects/branches/src/main.rs
+++ Rust/projects/branches/src/main.rs
@@ -3,8 +3,9 @@ fn main()
     let a = [10,20,30,40,50];
     let mut index = 0;
 
-    for element in a.iter()
+    for number in (1..4)
     {
-        println!("The value is: {}", element);
+        println!("{}!", number);
     }
+    println!("Liftoff!");
 }
