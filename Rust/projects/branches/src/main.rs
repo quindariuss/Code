9013ccef2b634ac8822835946d7b5b42d33ebf47
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-18 12:07:46 -0400

    inital commit

--- Rust/projects/branches/src/main.rs
+++ Rust/projects/branches/src/main.rs
@@ -1,11 +1,4 @@
 fn main()
 {
-    let a = [10,20,30,40,50];
-    let mut index = 0;
-
-    for number in (1..4)
-    {
-        println!("{}!", number);
-    }
-    println!("Liftoff!");
+    print("Chapter finished");
 }
