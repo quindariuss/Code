d8583c85de0ff78c413483546dc92e6eb0a05f24
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-18 14:00:55 +0000

    multiple conditions

--- Rust/projects/branches/src/main.rs
+++ Rust/projects/branches/src/main.rs
@@ -1,8 +1,21 @@
-fn main() {
-    let number = 7;
+fn main()
+{
+    let number = 6;
 
-    if number
+    if number % 4 == 0
     {
-        println!("number was three");
+        println!("number is divisable by 4");
     }
+        else if number % 3 == 0
+        {
+            println!("number is divisable by 3");
+        }
+            else if number % 2 == 0
+            {
+                println!("number is divisable by 2");
+            }
+                else
+                {
+                    println!("the numbner is not divisable by 4,3, or 2");
+                }
 }
