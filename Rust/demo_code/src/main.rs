ae3bdbd80fd4c64138857e2d92bdba768b1a2007
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-19 16:58:49 +0000

    ownership begining

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,7 +1,19 @@
 fn main()
 {
-    let x = 5;
-    let y = x;
+    let s = String::from("hello");
 
-    println!("x = {}, y = {}", x,y);
+    takes_ownership(s);
+
+    let x = 5;
+
+    makes_copy(x);
+}
+
+fn takes_ownership(some_string: String)
+{
+    println!("{}", some_string);
+}
+fn makes_copy(some_integer: i32)
+{
+    println!("{}", some_integer);
 }
