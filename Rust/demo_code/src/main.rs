bd4c57145a5f59b7cfd999c2f90b1bd87e231458
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-19 16:54:25 +0000

    same example with varibles

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,6 +1,7 @@
 fn main()
 {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
-    println!("s1 is {} and s2 is {]",s1, s2);
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x,y);
 }
