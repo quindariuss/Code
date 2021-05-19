03dc7f75005654f927d9f9870b4e4be8977e82f5
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-19 17:31:18 +0000

    tuples with ownersip

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,20 +1,14 @@
 fn main()
 {
-    let s1 = gives_ownership();
+    let s1 = String::from("hello");
+    let (s2, len ) = calculate_length(s1);
 
-    let s2 = String::from("hello");
-
-    let s3 = takes_and_gives_back(s2);
+    println!("the length of '{}' is {}", s2, len);
 }
 
-fn gives_ownership() -> String
+fn caclulate_length(s: String) -> (String, usize)
 {
-    let some_string = String::from("hello");
+    let length = s.len();
 
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String
-{
-    a_string
+    (s, length)
 }
