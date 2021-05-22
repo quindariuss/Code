0bd1f780487d4574b74bf3c659558d237adbd0e0
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-22 14:26:36 +0000

    error is immutable reference to something, we cannot also take a mutable reference.

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,8 +1,20 @@
 fn main(){
-    let s = String::from("hello");
+    let mut s = String::from("hello world");
 
-    let len = s.len();
-    println!("{}",len);
-    let slice = &s[3..];
-    println!("The substring from index 3 to the end is {}",slice);
+    let word = first_word(&s.copy());
+
+    s.clear();
+
+    println!("The first word is {}", word);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate(){
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
