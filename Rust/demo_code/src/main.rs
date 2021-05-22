38391d62f091e6475eb89483558c633637af8385
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-22 14:15:11 +0000

    done with just using ..

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -4,5 +4,5 @@ fn main(){
     let len = s.len();
     println!("{}",len);
     let slice = &s[3..len];
-    println!("The length from index 3 to the end is {}",slice);
+    println!("The substring from index 3 to the end is {}",slice);
 }
