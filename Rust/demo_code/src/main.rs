c6c3236e54a11524062e804a6981309c62e44374
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-23 21:38:04 +0000

    Complies in the main function

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -8,3 +8,10 @@ struct User {
     sign_in_count: u64,
     active: bool,
 }
+
+let user1 = User {
+    email: String::from("me@quindarius.com"),
+    username: String::from("quindarius"),
+    active: true,
+    sign_in_count: 1,
+};
