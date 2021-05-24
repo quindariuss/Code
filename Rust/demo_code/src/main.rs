5e3608bdc29ed932599a234af43b7514e1fd8bbb
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-24 12:42:59 +0000

    Using Tuple Structs Without Named Fields to Create Different Types

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,33 +1,6 @@
-fn main(){
-let mut user1 = User {
-    email: String::from("me@quindarius.com"),
-    username: String::from("quindarius"),
-    active: true,
-    sign_in_count: 1,
-};
-
-let user2 = User{
-    email: String::from("bob.com"),
-    username: String::from("anotherusername"),
-    active: user1.active,
-    sign_in_count: user1.sign_in_count,
-}
-
-user1.email = String::from("notme@quindarius.com")
-}
-
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User{
-        email: email,
-        username: username,
-        active: true,
-        sign_in_count: 1,
-    }
+struct Color(i32,i32,i32);
+struct Point(i32,i32,i32);
+fn main ()
+{let black = Color(0,0,0);
+let origin = Point(0,0,0);
 }
