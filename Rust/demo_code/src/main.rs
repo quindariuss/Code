0e1997369ce950b57d5a6f9a893cfcd653c591e7
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-24 12:36:43 +0000

    Creating Instances from Other Instances with Struct Update Syntax

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -16,3 +16,11 @@ struct User {
     active: bool,
 }
 
+fn build_user(email: String, username: String) -> User {
+    User{
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
