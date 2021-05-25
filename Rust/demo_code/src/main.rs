5202dfc865b1f4c36dc24d07c8d104e4f2289d3a
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-25 13:18:28 +0000

    debugging technique

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -2,11 +2,11 @@ fn main() {
     let rect1 = Rectangle {width: 30, height: 50};
 
     println!("The area of the rectangle is {}", area(&rect1));
+    println!("rect1 is {}", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32{
     rectangle.width * rectangle.height
-    println!("rect1 is {}", rect1);
 }
 
 struct Rectangle{
