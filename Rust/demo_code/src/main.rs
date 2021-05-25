768887f6c2b3fc8f21494daa4805c982c9d93199
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-25 13:16:47 +0000

    cant do this because rectangle has no defualt formatter

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
     let rect1 = Rectangle {width: 30, height: 50};
 
-    println!("The area of the rectangle is {}", area(rect1));
+    println!("The area of the rectangle is {}", area(&rect1));
 }
 
 fn area(rectangle: &Rectangle) -> u32{
     rectangle.width * rectangle.height
+    println!("rect1 is {}", rect1);
 }
 
 struct Rectangle{
