17d867f4a79a190b879547e8370117e1b3b1385d
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-26 12:33:18 +0000

    implementing methods with mroe parameters

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,15 +1,28 @@
 fn main() {
     let rect1 = Rectangle {width: 30, height: 50};
+    let rect2 = Rectangle {width: 10, height: 40};
+    let rect3 = Rectangle {width: 60, height: 45};
 
-    println!("The area of the rectangle is {}", area(&rect1));
-    println!("rect1 is {:?}", rect1);
+    println!("The area of the rectangle is {}", rect1.area());
+    println!("Cant rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Cant rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
 }
 
-fn area(rectangle: &Rectangle) -> u32{
-    rectangle.width * rectangle.height
-}
+
 #[derive(Debug)]
 struct Rectangle{
     width: u32,
     height: u32,
+}
+
+impl Rectangle{
+
+fn area(&self) -> u32{
+    self.width * self.height
+}
+fn can_hold(&self, other: &Rectangle)-> bool{
+    self.width > other.width && self.height > other.height
+}
+
 }
