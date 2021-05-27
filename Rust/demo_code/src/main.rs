13697f4c895772379250b723641573c18e8c5f78
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-27 12:57:00 +0000

    refactored to simply use enums

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,23 +1,10 @@
 enum IpVersion{
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
-struct IpAddress{
-    kind: IpVersion,
-    address: String,
-}
+
 fn route(ip_version: IpVersion){ }
 fn main() {
-    let home = IpAddress {
-        kind: IpVersion::V4,
-        address: String::from("127.0.0.1"),
-    };
-    let loopback = IpAddress{
-        kind: IpVersion::V6
-        address: String::from("::1"),
-    };
-    let four = IpVersion::V4;
-    let six = IpVersion::V6;
-    route(IpVersion::V4);
-    route(IpVersion::V6);
+    let home = IpVersion::V4(String::from("127.0.0.1"));
+    let loopback = IpVersion::V6(String::from("::1"));
 }
