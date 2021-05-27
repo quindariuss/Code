88de4635976be954607dd791a358dfe724331ef6
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-27 12:58:50 +0000

    refactored to have the v4 types into distrinct items

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,10 +1,10 @@
 enum IpVersion{
-    V4(String),
+    V4(u8,u8,u8,u8),
     V6(String),
 }
 
 fn route(ip_version: IpVersion){ }
 fn main() {
-    let home = IpVersion::V4(String::from("127.0.0.1"));
+    let home = IpVersion::V4(127,0,0,1);
     let loopback = IpVersion::V6(String::from("::1"));
 }
