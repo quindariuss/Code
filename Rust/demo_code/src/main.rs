e0d29a1e560db37741c3f8093dd8758b378d6886
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-27 12:38:11 +0000

    routing enums through functions

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,36 +1,11 @@
+enum IpVersion{
+    V4,
+    V6,
+}
+fn route(ip_version: IpVersion){ }
 fn main() {
-    let rect1 = Rectangle {width: 30, height: 50};
-    let rect2 = Rectangle {width: 10, height: 40};
-    let rect3 = Rectangle {width: 60, height: 45};
-    let sq = Rectangle::square(3);
-
-    println!("The area of the rectangle is {}", rect1.area());
-    println!("Cant rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Cant rect1 hold rect3? {}", rect1.can_hold(&rect3));
-    println!("The area of the square is {}",sq.area());
-
-}
-
-
-#[derive(Debug)]
-struct Rectangle{
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle{
-
-fn area(&self) -> u32{
-    self.width * self.height
-}
-fn can_hold(&self, other: &Rectangle)-> bool{
-    self.width > other.width && self.height > other.height
-}
-
-}
-
-impl Rectangle {
-    fn square(size: u32) -> Rectangle{
-        Rectangle {width: size, height: size}
-    }
+    let four = IpVersion::V4;
+    let six = IpVersion::V6;
+    route(IpVersion::V4);
+    route(IpVersion::V6);
 }
