5e7902a969b48359bdb3fccc615435336bfbd9a6
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-05-27 12:52:36 +0000

    struct and an enum

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -2,8 +2,20 @@ enum IpVersion{
     V4,
     V6,
 }
+struct IpAddress{
+    kind: IpVersion,
+    address: String,
+}
 fn route(ip_version: IpVersion){ }
 fn main() {
+    let home = IpAddress {
+        kind: IpVersion::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddress{
+        kind: IpVersion::V6
+        address: String::from("::1"),
+    };
     let four = IpVersion::V4;
     let six = IpVersion::V6;
     route(IpVersion::V4);
