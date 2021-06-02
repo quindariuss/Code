e90c4ec4b0b0f65be72c5883963bbd96206fda6a
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-02 18:49:48 +0000

    getting text on the page

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,10 +1,8 @@
-enum Message{
-    Quit,
-    Move{ x: i32, y: i32},
-    Write(String),
-    ChangeColor (i32, i32, i32),
-}
+enum Option<T> { Some(T), None, }
 
-fn main(){
-    println!("Hello World");
+fn main() {
+  let some_number = Some(5);
+  let some_string = Some("a string");
+
+  let absent_number: Option<i32> = None;
 }
