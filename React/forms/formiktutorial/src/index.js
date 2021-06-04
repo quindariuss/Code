d7069f5df590a3fe6d1b1452240646f1244174a7
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-04 11:47:27 +0000

    working email form

--- React/forms/formiktutorial/src/index.js
+++ React/forms/formiktutorial/src/index.js
@@ -1,4 +1,3 @@
-import react from 'react'
 import reactdom from 'react-dom'
 import
 {
@@ -8,12 +7,38 @@ import
 	Link,
 } from '@chakra-ui/react'
 
+import { useFormik } from 'formik'
+
+const SignupForm = () => {
+	const formik = useFormik({
+		initialValues:{
+			email:'',
+		},
+		onSubmit: values => {
+			alert(JSON.stringify(values, null,2));
+		},
+	});
+
+	return(
+		<form onSubmit={formik.handleSubmit}>
+			<label htmlFor="email">Email Address</label>
+			<input
+				id="email"
+				name="email"
+				type="email"
+				onChange={formik.handleChange}
+				value={formik.values.email}
+			/>
+			<button type="submit">Submit</button>
+		</form>
+	)
+}
+
+
 reactdom.render
 (
 	<ChakraProvider>
-		<Heading>
-			Hello World
-		</Heading>
+		<SignupForm/>
 	</ChakraProvider>,
 	document.getElementById('root')
 )
