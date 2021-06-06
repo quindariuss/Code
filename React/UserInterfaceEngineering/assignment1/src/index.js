63eb2390a66d3474c6ebb4b57c97be7bf6cd839e
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-06 13:20:25 +0000

    checking for null values

--- React/UserInterfaceEngineering/assignment1/src/index.js
+++ React/UserInterfaceEngineering/assignment1/src/index.js
@@ -5,15 +5,39 @@ import
 	ChakraProvider,
 	Heading,
 	Text,
+	Input,
+	Box,
+	Center,
 	Link,
+	Button,
 } from '@chakra-ui/react'
 
 reactdom.render
 (
 	<ChakraProvider>
+	<div>
 		<Heading>
-			Hello World
+			<Center p='10'>
+				Hello Word Explorer
+			</Center>
 		</Heading>
+		<Center>
+			<form action="/" method="get">
+				<Input
+				placeholder="search"
+				name="s"
+				/>
+				<Center p='10'>
+				<Button type="reset">Reset Search</Button>
+			</Center>
+			</form>
+		</Center>
+		<Center>
+		<Box width="lg" height="lg" bg="turquoise" borderRadius='10' border='2'>
+			Center
+		</Box>
+	</Center>
+	</div>
 	</ChakraProvider>,
 	document.getElementById('root')
 )
--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,8 +1,8 @@
 enum Option<T> { Some(T), None, }
 
 fn main() {
-  let some_number = Some(5);
-  let some_string = Some("a string");
+  let x: <i8> = 5;
+  let y: Option<i8> = Some(5);
 
-  let absent_number: Option<i32> = None;
+  let sum = x + y;
 }
