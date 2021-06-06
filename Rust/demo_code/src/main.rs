d7764d764a1843bf92b2e51fe70d2c38beab42d6
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-06 14:14:35 +0000

    yay the enum is working with the coins

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,8 +1,30 @@
-enum Option<T> { Some(T), None, }
+enum Coin{
+  Penny,
+  Nickel,
+  Dime,
+  Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState{
+  Alabama,
+  Georgia,
+  Mississippi,
+  Kansas,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+  match coin {
+  Coin::Penny => 1,
+  Coin::Nickel => 5,
+  Coin::Dime => 10,
+  Coin::Quarter(state) =>{
+    println!("State Quarter from {:?}!", state);
+    25;
+    },
+  }
+}
 
 fn main() {
-  let x: <i8> = 5;
-  let y: Option<i8> = Some(5);
-
-  let sum = x + y;
+value_in_cents(Coin::Quarter(UsState::Alabama));
 }
