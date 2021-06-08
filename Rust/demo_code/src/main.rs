6400c4d77813857cc8153a50f5f9259f266c3385
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-08 15:46:30 +0000

    Doing concise control flow with if let

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,10 +1,6 @@
 fn main() {
-  let some_u8_value = 0u8;
-  match some_u8_value {
-    1 => println!("one"),
-    3 => println!("three"),
-    5 => println!("five"),
-    7 => println!("seven"),
-    _ => (),
+  let some_u8_value = Some(0u8);
+  if let Some(3) = some_u8_value{
+    println!("3");
   }
 }
