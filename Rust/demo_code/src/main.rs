657f4d2dfe20ec0e53311dab2dff68d11109ea99
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-08 15:30:25 +0000

    Matchign with the option<t>

--- Rust/demo_code/src/main.rs
+++ Rust/demo_code/src/main.rs
@@ -1,30 +1,12 @@
-enum Coin{
-  Penny,
-  Nickel,
-  Dime,
-  Quarter(UsState),
-}
-
-#[derive(Debug)]
-enum UsState{
-  Alabama,
-  Georgia,
-  Mississippi,
-  Kansas,
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-  match coin {
-  Coin::Penny => 1,
-  Coin::Nickel => 5,
-  Coin::Dime => 10,
-  Coin::Quarter(state) =>{
-    println!("State Quarter from {:?}!", state);
-    25
-    },
+fn plus_one(x: Option<i32>) -> Option<i32> {
+  match x{
+    None => None,
+    Some(i) = Some(i + 1),
   }
 }
 
 fn main() {
-value_in_cents(Coin::Quarter(UsState::Alabama));
+  let five = Some(5);
+  let six = plus_one(five);
+  let none = plus_one(None);
 }
