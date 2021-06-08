e255d928dbe4b3c4cc06f1c2a47fc2aa8df1e271
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-08 16:00:18 +0000

    oops what I said last commit

--- Rust/restaurant/src/lib.rs
+++ Rust/restaurant/src/lib.rs
@@ -5,3 +5,19 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist(){}
+
+        fn seat_at_table(){}
+    }
+
+    mod serving{
+        fn take_order(){}
+
+        fn serve_order(){}
+
+        fn take_payment(){}
+    }
+}
