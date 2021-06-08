d3130d19ff95be1147cdd41512abb4d564ee794b
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-08 15:59:38 +0000

    defining modules to control scope and privacy

---
+++ Rust/restaurant/target/debug/deps/restaurant-e9610c640e27693c.d
@@ -0,0 +1,7 @@
+/Users/quin/Documents/Code/Rust/restaurant/target/debug/deps/restaurant-e9610c640e27693c.rmeta: src/lib.rs
+
+/Users/quin/Documents/Code/Rust/restaurant/target/debug/deps/librestaurant-e9610c640e27693c.rlib: src/lib.rs
+
+/Users/quin/Documents/Code/Rust/restaurant/target/debug/deps/restaurant-e9610c640e27693c.d: src/lib.rs
+
+src/lib.rs:
---
+++ Rust/restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/Users/quin/Documents/Code/Rust/restaurant/target/debug/librestaurant.rlib: /Users/quin/Documents/Code/Rust/restaurant/src/lib.rs
