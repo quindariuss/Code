7c12b78591bdafcd4f7e30828c2649a20b322ef3
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-06-08 15:54:26 +0000

    new project

---
+++ Rust/restaurant/target/rls/debug/deps/restaurant-6e9aa23516115d32.d
@@ -0,0 +1,5 @@
+/Users/quin/Documents/Code/Rust/restaurant/target/rls/debug/deps/restaurant-6e9aa23516115d32.rmeta: src/lib.rs
+
+/Users/quin/Documents/Code/Rust/restaurant/target/rls/debug/deps/restaurant-6e9aa23516115d32.d: src/lib.rs
+
+src/lib.rs:
---
+++ Rust/restaurant/target/rls/debug/deps/restaurant-8dddd2b2907d7c76.d
@@ -0,0 +1,5 @@
+/Users/quin/Documents/Code/Rust/restaurant/target/rls/debug/deps/restaurant-8dddd2b2907d7c76.rmeta: src/lib.rs
+
+/Users/quin/Documents/Code/Rust/restaurant/target/rls/debug/deps/restaurant-8dddd2b2907d7c76.d: src/lib.rs
+
+src/lib.rs:
