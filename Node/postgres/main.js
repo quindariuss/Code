ef0b76da73c57f81a08bc50980dc11532ca876f3
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-07-15 17:12:59 +0000

    getting output from the database

--- Node/postgres/main.js
+++ Node/postgres/main.js
@@ -1,12 +1,19 @@
-const {Client} = require('pg')
-var cs = "postgres://quindarius:root@localhost:5432/quin"
+const { Client } = require("pg");
+var cs = "postgres://quin:root@localhost:5432/quin";
 
-const client = new Client (
-	{
-	connectionString: cs
-	}
-)
-client.query('select * from accounts' , (err, res) => {
-console.log(err ? err.stack : res)
-	client.end()
-})
+const client = new Client({
+  connectionString: cs,
+});
+
+client.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log("connected");
+  }
+});
+
+client.query("select * from accounts", (err, res) => {
+  console.log(err ? err.stack : res);
+  client.end();
+});
