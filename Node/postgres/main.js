d1cd303af2e04e6742b129bdd99282b5a6923945
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-07-15 21:34:52 +0000

    finished hitting the api

--- Node/postgres/main.js
+++ Node/postgres/main.js
@@ -1,4 +1,24 @@
 const { Client } = require("pg");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors({ origin: "http://localhost:62683" }));
+// routes will go here
+app.post("/api", function (req, res) {
+  const email = req.query.email;
+  const password = req.query.password;
+
+  res.send({
+    status: validateUser(email, password),
+  });
+});
+
+app.listen(port);
+console.log("Server started at http://localhost:" + port);
+
 var cs = "postgres://quin:root@localhost:5432/quin";
 
 const client = new Client({
@@ -13,7 +33,19 @@ client.connect((err) => {
   }
 });
 
-client.query("select * from accounts", (err, res) => {
-  console.log(err ? err.stack : res);
+var results;
+
+client.query("select * from account", (err, res) => {
+  console.log(err ? err.stack : (results = res.rows[0]));
   client.end();
 });
+
+console.log({ results });
+
+function validateUser(email, password) {
+  if (email == "quindarius" && password == "123") {
+    return "valid user";
+  } else {
+    return "invalid user";
+  }
+}
